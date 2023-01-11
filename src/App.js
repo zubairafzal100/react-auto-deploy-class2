c8f8e718570fedd1b28daaeef99b62f3e81a15cb
zubairafzal100 <zubairafzal100@gmail.com>
2023-01-11 15:26:44 +0400

    updated app.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World React from Zubair Afzal</h1>
+      <h1>Hello World React from Zubair</h1>
     </div>
   );
 }
